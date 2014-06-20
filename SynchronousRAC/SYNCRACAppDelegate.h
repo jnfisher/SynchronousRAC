054dc0ba3e6d48408d5e04bca3e37db2eb03e480
John Fisher <john.fisher@atomicobject.com>
2014-06-19 21:27:25 -0400

    Initial Commit

---
+++ SynchronousRAC/SYNCRACAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SYNCRACAppDelegate.h
+//  SynchronousRAC
+//
+//  Created by John Fisher on 6/19/14.
+//  Copyright (c) 2014 John Fisher. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SYNCRACAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SynchronousRAC/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SynchronousRAC
+//
+//  Created by John Fisher on 6/19/14.
+//  Copyright (c) 2014 John Fisher. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SYNCRACAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([SYNCRACAppDelegate class]));
+    }
+}
